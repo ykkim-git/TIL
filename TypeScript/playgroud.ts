cb25a1bc54956cb4aed0713b2c5b7c687ae56e34
ykkim <rladudrnjs12@gmail.com>
2022-09-23 08:14:40 +0900

    옵셔널 파라미터 내용 수정

--- TypeScript/playgroud.ts
+++ TypeScript/playgroud.ts
@@ -1,7 +1,2 @@
-function combine(input1: number, input2: number) {
-  const result = input1 + input2;
-  return result;
-}
-
-const combinedAges = combine(30, 26);
-console.log(combinedAges);
+const log = (a: string, b?: string, c: number) => {};
+log("hello", "world", 1); // 정상 호출
